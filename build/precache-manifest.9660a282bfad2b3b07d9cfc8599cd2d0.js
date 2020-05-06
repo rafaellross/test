4d59f8ef9cac1c9193476c9dd838bbcf862b06d4
Rafael Ross <rafaellross@gmail.com>
2020-05-07 07:34:17 +1000

    first commit

---
+++ build/precache-manifest.9660a282bfad2b3b07d9cfc8599cd2d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff55187a85e2f081e45d802e0e091ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac803ac76d27a8ddf707",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "841a8a994cda0bc4b9bc",
+    "url": "/static/js/2.7334ada5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7334ada5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac803ac76d27a8ddf707",
+    "url": "/static/js/main.d8268eaf.chunk.js"
+  },
+  {
+    "revision": "06fc8f603d98e70eec1c",
+    "url": "/static/js/runtime-main.da8e370a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
